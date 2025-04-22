7e61cb27201fc8b3dfee5105a8f61c55aeda8a07
tijn <tijnjh@proton.me>
2025-04-22 21:15:49 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,8 @@ declare module 'hug-text' {
     /**
      * Adjusts the width of a given element to fit its text content without increasing its height.
      * @param {HTMLElement | NodeList | HTMLCollection | HTMLElement[]} element - The element to adjust.
+     * @deprecated
+     * This function is now part of https://tijn.dev/tsuite
      */
     export function hugText(element: HTMLElement | NodeList | HTMLCollection | HTMLElement[]): void;
   }
